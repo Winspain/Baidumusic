66ae2a45235ded8544df01dee59182e4ed1528d1
winspain <penndicardo@outlook.com>
2018-01-18 23:08:54 +0800

    V2.0更新UI界面 V3.0新增progressBar，文件存储路径 V3.0问题：多次点击下载按键不会清空歌名与进度条进度

--- spider.py
+++ spider.py
@@ -4,10 +4,8 @@
 # @File    : spider.py
 # @Software: PyCharm
 import sys
-import requests
 import re
-import json
-from PyQt4 import QtCore, QtGui, uic
+from PyQt4 import QtGui
 from BD import Ui_MainWindow
 
 class baiduMusic(QtGui.QMainWindow,Ui_MainWindow):
@@ -18,18 +16,42 @@ class baiduMusic(QtGui.QMainWindow,Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.dLoad)
+        self.pushButton_click.clicked.connect(self.filePath)    #注意不要写成filepath()实例化
 
     def dLoad(self):
         from threads import SpiderThread
         singers = self.lineEdit.text()
-        self.bwThread = SpiderThread(str(singers))
+        re_path = self.lineEdit_path.text()
+        download_num = self.lineEdit_download.text()
+        self.bwThread = SpiderThread(str(singers),str(re_path),str(download_num))
+        #SpiderThread的初始化函数中加入参数，就可以再线程中直接加入变量
         self.bwThread.singerSignal.connect(self.pInfo)
+        self.bwThread.singerSignal.connect(self.slotStart) #将每次mp3_name的信号返回
+        self.bwThread.numSignal.connect(self.numInfo)
         self.bwThread.start()
-    def pInfo(self,ls,tsongs):
+
+    '''打印歌曲名信息'''
+    def pInfo(self,ls):
         self.textBrowser.setText(ls)
-        self.lineEdit_songs.setText(tsongs)
 
+    '''打印歌曲总数目'''
+    def numInfo(self,ls):
+        self.lineEdit_songs.setText(ls)
 
+    '''打印文件路径'''
+    def filePath(self):
+        relate_path = QtGui.QFileDialog.getExistingDirectory(self,'open file')
+        self.lineEdit_path.setText(relate_path + '\\')  #给末尾加上\，使文件夹路径正确
+
+    '''进度条'''
+    def slotStart(self):
+        num = int(self.lineEdit_download.text())
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(num)
+        countNum2 = self.textBrowser.toPlainText()  #实时的mp3_name实现进度条动态效果
+        countNums = len(re.findall(r',',countNum2))+1   #将字符串中的逗号匹配出来以确定元素个数
+        for self.countNum in range(num):
+            self.progressBar.setValue(countNums)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
